47a5a206c0b35fc356492ea8b3f030a839740882
vinsensiuskurniaputra <vinsensiuskurniaputra01@gmail.com>
2025-07-10 20:03:58 +0700

    Update dependencies in go.mod and go.sum; add CORS middleware to router

--- infrastructure/router/router.go
+++ infrastructure/router/router.go
@@ -1,18 +1,31 @@
 package router
 
 import (
+	"time"
+
 	userManagementHandler "github.com/3-Orang-IT/tekna-erp-api/internal/admin/interface/handler"
 	adminRepository "github.com/3-Orang-IT/tekna-erp-api/internal/admin/interface/repository"
 	adminUsecase "github.com/3-Orang-IT/tekna-erp-api/internal/admin/usecase"
 	"github.com/3-Orang-IT/tekna-erp-api/internal/auth/interface/handler"
 	"github.com/3-Orang-IT/tekna-erp-api/internal/auth/interface/repository"
 	"github.com/3-Orang-IT/tekna-erp-api/internal/auth/usecase"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 func InitRoutes(db *gorm.DB) *gin.Engine {
 	r := gin.Default()
+
+	// Add CORS middleware
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge: 12 * time.Hour,
+	}))
 
 	// Init repository dan usecase
 	authRepo := repository.NewAuthRepository(db)
