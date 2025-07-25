5782551c2e3c1890b58721b3f60cc5778e67fa88
vinsensiuskurniaputra <vinsensiuskurniaputra01@gmail.com>
2025-07-25 09:34:29 +0700

    Refactor Province Management: update repository and use case to include search functionality in GetProvinces method and add GetProvinceEditPage handler for improved province management.

--- internal/admin/domain/province_management_repository.go
+++ internal/admin/domain/province_management_repository.go
@@ -3,9 +3,9 @@ package adminRepository
 import "github.com/3-Orang-IT/tekna-erp-api/internal/common/entity"
 
 type ProvinceManagementRepository interface {
-	CreateProvince(province *entity.Province) error
-	GetProvinces(page, limit int) ([]entity.Province, error)
-	GetProvinceByID(id string) (*entity.Province, error)
-	UpdateProvince(id string, province *entity.Province) error
-	DeleteProvince(id string) error
+	   CreateProvince(province *entity.Province) error
+	   GetProvinces(page, limit int, search string) ([]entity.Province, error)
+	   GetProvinceByID(id string) (*entity.Province, error)
+	   UpdateProvince(id string, province *entity.Province) error
+	   DeleteProvince(id string) error
 }
