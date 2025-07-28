0af44dcb31f638418d275f10bfed67e1a85e7433
vinsensiuskurniaputra <vinsensiuskurniaputra01@gmail.com>
2025-07-28 23:09:29 +0700

    fix: remove unnecessary blank line in CreateEmployeeWithUserInput struct

--- internal/admin/interface/dto/employee_dto.go
+++ internal/admin/interface/dto/employee_dto.go
@@ -29,7 +29,7 @@ type CreateEmployeeWithUserInput struct {
 	Telp            string `json:"telp"`
 	PhotoProfileURL string `json:"photo_profile_url"`
 	RoleIDs         []uint `json:"roles" binding:"required"`
-	
+
 	// Employee data
 	JobPositionID    uint   `json:"job_position_id" binding:"required"`
 	DivisionID       uint   `json:"division_id" binding:"required"`
